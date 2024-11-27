8c2c7fc0b765af3555b25818ed2be3a302c8ba19
AlessandroTraverso <alino.traverso@gmail.com>
2024-11-27 13:21:41 +0100

    Modifica

--- Libro.py
+++ Libro.py
@@ -14,30 +14,29 @@ def AggiuntaLibro(lista):
 def PrestitoLibro(lista,prestati):
     print("Quanti libri vuoi prendere in prestito?")
     x = int(input())
-    for i in range (x):
+    for i in range(x):
         print("Inserisci il titolo del libro numero" ,i+1, "che vuoi prendere in prestito")
         titolo = input()
-        for ind in range(len(lista)):
-            if(lista[ind]==titolo):
-                prestati.append(titolo)
-                lista.remove(titolo)
-            else:
-                print(f"Purtroppo il libro " + titolo + " non è al momento disponibile")
+        if titolo in lista:
+            prestati.append(titolo)
+            lista.remove(titolo)
+        else:
+            print(f"Purtroppo il libro " + titolo + " non è al momento disponibile")
     print(prestati)
     print(lista)
 
 def RestituzioneLibro(lista,prestati):
     print("Quanti libri vuoi restituire?")
     x = int(input())
+    y = len(prestati)
     for i in range (x):
         print("Inserisci il titolo del libro numero" ,i+1, "che vuoi restituire")
         titolo = input()
-        for ind in range(len(prestati)):
-            if(prestati[ind]==titolo):
-                prestati.remove(titolo)
-                lista.append(titolo)
-            else:
-                print("Il libro dal titolo " + titolo + " non risulta prestato")
+        if titolo in prestati:
+            prestati.remove(titolo)
+            lista.append(titolo)
+        else:
+            print("Il libro dal titolo " + titolo + " non risulta prestato")
     lista.sort()
     print(lista)
     print(prestati)
--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 from Libro import AggiuntaLibro
 from Libro import PrestitoLibro
+from Libro import RestituzioneLibro
 
 print("Benvenuto nella libreria!")
 listalibri = []
 libriprestati = []
 AggiuntaLibro(listalibri)
 PrestitoLibro(listalibri,libriprestati)
+RestituzioneLibro(listalibri,libriprestati)
