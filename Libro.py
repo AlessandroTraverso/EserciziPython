0d79f20d766734148f69f30ec93a23572ba5c321
AlessandroTraverso <alino.traverso@gmail.com>
2024-11-27 12:08:33 +0100

    Modifica

--- Libro.py
+++ Libro.py
@@ -4,7 +4,7 @@ def AggiuntaLibro(lista):
     print("Quanti libri vuoi inserire all'interno della libreria?")
     x = input()
     for i in x:
-        titolo = (input(print(f"Inserisci il titolo del libro numero" ,i+1)))
+        titolo = (input(print(f"Inserisci il titolo del libro numero")))
         lista.append(titolo)
     lista.sort()
     print("Questa è la tua libreria in ordine alfabetico:")
